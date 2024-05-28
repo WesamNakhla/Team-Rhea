edb5ab640caf4b31b1407fa7f2e84989f9bc281c
AhmedAlAboud <ahmed.aboud1991@gmail.com>
2024-05-28 07:13:56 -0700

    import os
    import os

--- import_frame.py
+++ import_frame.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
 from tkinterdnd2 import TkinterDnD, DND_FILES
+import os
 
 class ImportFrame(ttk.Frame):
     def __init__(self, parent, app):
