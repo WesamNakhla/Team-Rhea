37fcf5921fb08dc4abb2342eee01e55e8e32014c
Mikael Björkli <mikaelbjorkli01@gmail.com>
2024-06-06 13:13:58 +0200

    Added VOL3 version function
    Added a function that checks if path is selected , and when  you click save it runs a volatility3 command to check what version it is, it then stores the version in settings.json.

--- logic/settings_frame.py
+++ logic/settings_frame.py
@@ -1,6 +1,8 @@
 #located at logic/settings_frame.py
 import json
 import os
+import subprocess
+import re
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
@@ -22,10 +24,17 @@ class SettingsFrameLogic:
             'line_distance': self.line_distance_spinbox.get(),
             'letter_distance': self.letter_distance_spinbox.get()
         }
+        # Check the version of Volatility
+        version = self.check_volatility_version()
+        if version:
+            settings['volatility_version'] = version  # Store the version in settings
+
         with open('settings.json', 'w') as settings_file:
-            json.dump(settings, settings_file)
+            json.dump(settings, settings_file, indent=4)  # Pretty print JSON for readability
+
         self.original_settings = settings.copy()
-        messagebox.showinfo("Settings Saved", "Your settings have been saved.")
+        messagebox.showinfo("Settings Saved", "Your settings have been saved including Volatility version.")
+
 
     def load_settings(self):
         try:
@@ -53,6 +62,28 @@ class SettingsFrameLogic:
         self.letter_distance_spinbox.delete(0, tk.END)
         self.letter_distance_spinbox.insert(0, self.original_settings.get('letter_distance', 1))
 
+    def check_volatility_version(self):
+
+        vol_path = os.path.join(self.volatility_path_entry.get(), 'vol.py')
+    # Ensure the file exists before attempting to execute it
+        if not os.path.isfile(vol_path):
+            messagebox.showerror("Error", "Volatility executable not found at the specified path.")
+            return
+
+        try:
+        # Call the volatility script with '-h' to get output including the version
+            result = subprocess.run(['python', vol_path, '-h'], text=True, capture_output=True)
+            version_info = re.search(r'Volatility 3 Framework (\d+\.\d+\.\d+)', result.stdout)
+            if version_info:
+                version = version_info.group(1)
+                return version
+            else:
+                messagebox.showerror("Error", "Failed to retrieve Volatility version from the output.")
+                return None
+        except subprocess.CalledProcessError as e:
+            messagebox.showerror("Error", f"Failed to execute Volatility: {e}")
+            return None
+    
     def exit_settings(self):
         current_settings = {
             'volatility_path': self.volatility_path_entry.get(),
