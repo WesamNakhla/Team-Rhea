adff76205262c61d3264f59dfe3ede92249966c3
AhmedAlAboud <ahmed.aboud1991@gmail.com>
2024-06-07 01:14:44 +0200

    Infinite adjustment of center
    Infinite adjustment of center

--- ui/command_frame.py
+++ ui/command_frame.py
@@ -1,4 +1,3 @@
-# ui/command_frame.py
 import tkinter as tk
 from tkinter import ttk, Scrollbar, VERTICAL
 from logic.command_frame import CommandFrameLogic
@@ -11,108 +10,106 @@ class CommandFrame(tk.Frame, CommandFrameLogic):
         self.init_ui()
 
     def init_ui(self):
+        self.grid(sticky="nsew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        
         self.main_frame = tk.Frame(self)
-        self.main_frame.grid(row=0, column=0, sticky="nsew")
+        self.main_frame.grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
         self.main_frame.grid_columnconfigure(0, weight=1)
-        self.main_frame.grid_columnconfigure(1, weight=2)
-        self.main_frame.grid_columnconfigure(2, weight=1)
-
-        self.label = tk.Label(self.main_frame, text="Command management", font=('Arial', 28))
-        self.label.grid(row=0, column=0, columnspan=4, pady=10, padx=10, sticky="w")
-
-        self.add_form_ui()  # Add new form UI for adding commands
+        self.main_frame.grid_rowconfigure(1, weight=1)
+        
+        self.label = tk.Label(self.main_frame, text="Command Management", font=('Arial', 28))
+        self.label.grid(row=0, column=0, pady=20, sticky="n")
+        
+        self.add_form_ui()
+        
         self.command_frame = tk.Frame(self.main_frame)
-        self.command_frame.grid(row=2, column=0, columnspan=4, pady=10, padx=10, sticky="nsew")
+        self.command_frame.grid(row=2, column=0, pady=10, sticky="nsew")
         self.command_frame.grid_columnconfigure(0, weight=1)
-        self.command_frame.grid_columnconfigure(1, weight=1)
-        self.command_frame.grid_columnconfigure(2, weight=2)
-        self.command_frame.grid_columnconfigure(3, weight=1)
-
+        self.command_frame.grid_rowconfigure(0, weight=1)
+        
         self.scrollbar = Scrollbar(self.command_frame, orient=VERTICAL)
-        self.scrollbar.grid(row=0, column=4, sticky='ns')
-
+        self.scrollbar.grid(row=0, column=1, sticky='ns')
+        
         self.command_canvas = tk.Canvas(self.command_frame, yscrollcommand=self.scrollbar.set)
-        self.command_canvas.grid(row=0, column=0, columnspan=4, sticky="nsew")
+        self.command_canvas.grid(row=0, column=0, sticky="nsew")
         self.scrollbar.config(command=self.command_canvas.yview)
-
+        
         self.command_container = tk.Frame(self.command_canvas)
-        self.command_canvas.create_window((0, 0), window=self.command_container, anchor="nw")
-
+        self.command_canvas.create_window((0, 0), window=self.command_container, anchor="n")
+        
         self.command_container.bind(
             "<Configure>",
             lambda e: self.command_canvas.configure(scrollregion=self.command_canvas.bbox("all"))
         )
-
+        
         self.command_entries = []
         self.type_entries = []
         self.description_entries = []
-
+        
         self.load_commands_ui()
-
-        self.add_command_button = ttk.Button(self.main_frame, text="Add command", command=self.add_command_ui)
-        self.add_command_button.grid(row=3, column=0, columnspan=4, pady=10, padx=10, sticky="ew")
-
+        
+        self.add_command_button = ttk.Button(self.main_frame, text="Add Command", command=self.add_command_ui)
+        self.add_command_button.grid(row=3, column=0, pady=10, sticky="ew")
+        
         self.save_button = ttk.Button(self.main_frame, text="Save", command=self.save_commands)
-        self.save_button.grid(row=4, column=2, pady=10, padx=10, sticky="e")
-
+        self.save_button.grid(row=4, column=0, pady=10, padx=10, sticky="e")
+        
         self.exit_button = ttk.Button(self.main_frame, text="Cancel", command=self.app.switch_to_workspace_frame)
-        self.exit_button.grid(row=4, column=3, pady=10, padx=10, sticky="w")
+        self.exit_button.grid(row=4, column=0, pady=10, padx=10, sticky="w")
 
     def add_form_ui(self):
-        # New command input form at the top
         self.new_command_frame = tk.Frame(self.main_frame)
-        self.new_command_frame.grid(row=1, column=0, columnspan=4, pady=10, padx=10, sticky="nsew")
+        self.new_command_frame.grid(row=1, column=0, pady=10, sticky="ew")
         self.new_command_frame.grid_columnconfigure(0, weight=1)
         self.new_command_frame.grid_columnconfigure(1, weight=1)
-        self.new_command_frame.grid_columnconfigure(2, weight=2)
-
-        self.new_command_entry = ttk.Entry(self.new_command_frame, width=30)
-        self.new_command_entry.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
-        self.new_command_entry.insert(0, "Enter command")
-
-        self.new_type_entry = ttk.Entry(self.new_command_frame, width=30)
-        self.new_type_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
-        self.new_type_entry.insert(0, "Enter type")
-
-        self.new_description_entry = ttk.Entry(self.new_command_frame, width=50)
-        self.new_description_entry.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
-        self.new_description_entry.insert(0, "Enter description")
+        self.new_command_frame.grid_columnconfigure(2, weight=1)
+        
+        self.new_command_label = tk.Label(self.new_command_frame, text="Enter command", font=('Arial', 12))
+        self.new_command_label.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+        
+        self.new_type_label = tk.Label(self.new_command_frame, text="Enter type", font=('Arial', 12))
+        self.new_type_label.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+        
+        self.new_description_label = tk.Label(self.new_command_frame, text="Enter description", font=('Arial', 12))
+        self.new_description_label.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
 
     def load_commands_ui(self):
         for widget in self.command_container.winfo_children():
             widget.destroy()
-
+        
         self.command_entries.clear()
         self.type_entries.clear()
         self.description_entries.clear()
-
+        
         for index, command in enumerate(self.commands):
             self.create_command_row(index, command)
 
     def create_command_row(self, index, command):
-        command_entry = ttk.Entry(self.command_container, width=30)
+        command_entry = ttk.Entry(self.command_container, width=20)
         command_entry.grid(row=index, column=0, padx=5, pady=5, sticky="ew")
         command_entry.insert(0, command["command"])
         self.command_entries.append(command_entry)
-
-        type_entry = ttk.Entry(self.command_container, width=30)
+        
+        type_entry = ttk.Entry(self.command_container, width=20)
         type_entry.grid(row=index, column=1, padx=5, pady=5, sticky="ew")
         type_entry.insert(0, command["type"])
         self.type_entries.append(type_entry)
-
-        description_entry = ttk.Entry(self.command_container, width=50)
+        
+        description_entry = ttk.Entry(self.command_container, width=30)
         description_entry.grid(row=index, column=2, padx=5, pady=5, sticky="ew")
         description_entry.insert(0, command["description"])
         self.description_entries.append(description_entry)
-
+        
         delete_button = ttk.Button(self.command_container, text="x", command=lambda idx=index: self.delete_command_ui(idx))
         delete_button.grid(row=index, column=3, padx=5, pady=5)
 
     def add_command_ui(self):
         command = {
-            "command": self.new_command_entry.get(),
-            "type": self.new_type_entry.get(),
-            "description": self.new_description_entry.get()
+            "command": self.new_command_label.cget("text"),
+            "type": self.new_type_label.cget("text"),
+            "description": self.new_description_label.cget("text")
         }
         self.commands.append(command)
         self.load_commands_ui()
