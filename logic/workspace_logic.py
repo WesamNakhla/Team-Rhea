0ecd59d62daceaccd5a33d5d25c5bd84fa0800ac
Mikael Björkli <mikaelbjorkli01@gmail.com>
2024-06-11 17:09:51 +0200

    Added windows.module output and windows.module command

--- logic/workspace_logic.py
+++ logic/workspace_logic.py
@@ -1090,6 +1090,148 @@ class NetstatOutputFrame(tk.Frame):
         plt.title("Distribution of Connection Types")
         plt.show()
 
+class ModulesOutputFrame(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        # Define the columns for the Treeview
+        self.columns = ["Offset", "Base", "Size", "Name", "Path", "File output"]
+
+        self.tree = ttk.Treeview(self, columns=self.columns, show='headings')
+        self.tree.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+        # Configure the columns
+        for col in self.columns:
+            self.tree.heading(col, text=col, command=lambda _col=col: self.sort_treeview(_col, False))
+            self.tree.column(col, width=150, anchor="center")
+
+        self.scrollbar_y = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
+        self.scrollbar_y.grid(row=0, column=2, sticky="ns")
+        self.tree.configure(yscroll=self.scrollbar_y.set)
+
+        self.scrollbar_x = ttk.Scrollbar(self, orient="horizontal", command=self.tree.xview)
+        self.scrollbar_x.grid(row=1, column=0, columnspan=2, sticky="ew")
+        self.tree.configure(xscroll=self.scrollbar_x.set)
+
+        self.tree.bind("<Button-3>", self.show_context_menu)
+
+        self.context_menu = tk.Menu(self, tearoff=0)
+        self.context_menu.add_command(label="Copy", command=self.copy_selected)
+        self.context_menu.add_command(label="Export to CSV", command=self.export_to_csv)
+        self.context_menu.add_command(label="Show Bar Chart", command=self.show_bar_chart)
+
+        # Add buttons for exporting to CSV and showing the bar chart
+        self.export_button = ttk.Button(self, text="Export to CSV", command=self.export_to_csv)
+        self.export_button.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
+
+        self.chart_button = ttk.Button(self, text="Show Bar Chart", command=self.show_bar_chart)
+        self.chart_button.grid(row=2, column=1, padx=10, pady=5, sticky="ew")
+
+    def sort_treeview(self, col, reverse):
+        def convert(data):
+            try:
+                return int(data, 16) if '0x' in data else int(data)
+            except ValueError:
+                try:
+                    return float(data)
+                except ValueError:
+                    return data
+
+        data_list = [(self.tree.set(item, col), item) for item in self.tree.get_children('')]
+        data_list.sort(key=lambda t: convert(t[0]), reverse=reverse)
+
+        for index, (_, item) in enumerate(data_list):
+            self.tree.move(item, '', index)
+
+        self.tree.heading(col, command=lambda: self.sort_treeview(col, not reverse))
+
+    def populate(self, findings):
+        for i in self.tree.get_children():
+            self.tree.delete(i)
+        
+        lines = findings.strip().split("\n")
+        filtered_lines = []
+        for line in lines:
+            if line.strip() and not self.is_unwanted_line(line):
+                filtered_lines.append(line)
+        
+        for line in filtered_lines:
+            values = re.split(r'\s+', line.strip(), maxsplit=5)
+            values = self.adjust_columns(values)
+            if len(values) == len(self.columns):
+                self.tree.insert("", "end", values=values)
+
+    def adjust_columns(self, values):
+        if len(values) > len(self.columns):
+            values = values[:len(self.columns)]
+        elif len(values) < len(self.columns):
+            values += [''] * (len(self.columns) - len(values))
+        return values
+
+    def is_unwanted_line(self, line):
+        unwanted_keywords = ["Progress", "Scanning", "Stacking", "PDB scanning finished"]
+        return any(keyword in line for keyword in unwanted_keywords)
+
+    def show_context_menu(self, event):
+        try:
+            row_id = self.tree.identify_row(event.y)
+            self.tree.selection_set(row_id)
+            self.context_menu.post(event.x_root, event.y_root)
+        finally:
+            self.context_menu.grab_release()
+
+    def copy_selected(self):
+        try:
+            selected_items = self.tree.selection()
+            if not selected_items:
+                return
+
+            copied_text = ""
+            for item in selected_items:
+                row_values = self.tree.item(item, "values")
+                copied_text += "\t".join(row_values) + "\n"
+
+            self.clipboard_clear()
+            self.clipboard_append(copied_text)
+        except Exception as e:
+            print(f"Error copying to clipboard: {e}")
+
+    def export_to_csv(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".csv",
+                                                 filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
+        if file_path:
+            try:
+                with open(file_path, mode='w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(self.columns)
+                    for row in self.tree.get_children():
+                        row_values = self.tree.item(row, 'values')
+                        writer.writerow(row_values)
+                messagebox.showinfo("Export to CSV", f"Data successfully exported to {file_path}")
+            except Exception as e:
+                messagebox.showerror("Export to CSV", f"Error exporting to CSV: {e}")
+
+    def show_bar_chart(self):
+        sizes = [int(self.tree.item(item, "values")[2], 16) for item in self.tree.get_children()]
+        names = [self.tree.item(item, "values")[3] for item in self.tree.get_children()]
+
+        fig = Figure(figsize=(10, 6), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.bar(names, sizes, color='blue')
+        ax.set_xlabel('Kernel Module Name')
+        ax.set_ylabel('Size (Bytes)')
+        ax.set_title('Kernel Modules Sizes')
+        ax.tick_params(axis='x', rotation=45)
+
+        chart_window = tk.Toplevel(self)
+        chart_window.title("Kernel Module Size Bar Chart")
+        canvas = FigureCanvasTkAgg(fig, master=chart_window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
 class WorkspaceFrameLogic:
     def __init__(self, parent, file_handler):
         self.parent = parent
@@ -1234,6 +1376,11 @@ class WorkspaceFrameLogic:
 
         elif command_name == 'windows.netscan':
             pstree_frame = NetstatOutputFrame(new_tab)
+            pstree_frame.pack(expand=True, fill='both')
+            pstree_frame.populate(findings)
+        
+        elif command_name == 'windows.modules':
+            pstree_frame = ModulesOutputFrame(new_tab)
             pstree_frame.pack(expand=True, fill='both')
             pstree_frame.populate(findings)
 
